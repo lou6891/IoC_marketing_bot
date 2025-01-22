3dea6966befe4bb5acb922d2ddc6cf6deac43215
lou6891 <lucaconti19@gmail.com>
2025-01-22 18:44:07 +0100

    Changelog: - Created questionnaire to collect optician data - REfactor of the frontend code to be organized in components

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.agent import GPTAgent, generate_follow_up_suggestions
+from src.agent import GPTAgent
 from src.constants import (
     FRONTEND_HTML_STYLES,
     FRONTEND_BOT_ICON,
@@ -7,109 +7,11 @@ from src.constants import (
     FRONTEND_USER_ICON,
 )
 
-
-def process_and_display_gpt_streaming_response(
-    gpt_agent: GPTAgent, bot_icon: str
-) -> str:
-    assistant_placeholder = st.empty()
-    assistant_response = ""
-
-    with st.chat_message("assistant", avatar=bot_icon):
-        stream_response = gpt_agent.run(st.session_state["messages"])
-
-        for chunk in stream_response:
-            assistant_response += chunk
-            assistant_placeholder.markdown(assistant_response)
-
-    st.session_state["messages"].append(
-        {"role": "assistant", "content": assistant_response}
-    )
+from src.frontend_components.chat import chat_component
+from frontend_components.questionnaire import user_questioner
 
 
-def generate_follow_up_suggestion(gpt_agent: GPTAgent) -> str:
-    suggestions = generate_follow_up_suggestions(
-        gpt_agent, st.session_state["messages"]
-    )
-    st.session_state["follow_ups"] = suggestions
-
-
-def display_user_message_to_chat(chat_text: str, user_icon: str):
-    with st.chat_message("user", avatar=user_icon):
-        st.markdown(chat_text)
-
-
-def handle_and_display_follow_up_suggestions() -> None:
-    if st.session_state["follow_ups"]:
-        st.markdown("**Suggerimenti di follow-up:**")
-        for i, suggestion in enumerate(st.session_state["follow_ups"], start=1):
-            if st.button(f"{i}. {suggestion}", key=f"follow_up_{i}"):
-                # add follow up to pending user input, which is needed to run the agent with the follow up question
-                st.session_state["pending_user_input"] = suggestion
-
-                # reset follow up, update the frontend
-                st.session_state["follow_ups"] = []
-                st.rerun()
-
-
-def display_conversation_history(user_icon: str, bot_icon: str) -> None:
-    for message in st.session_state["messages"]:
-        if message["role"] == "system":
-            continue
-        with st.chat_message(
-            message["role"], avatar=user_icon if message["role"] == "user" else bot_icon
-        ):
-            st.markdown(message["content"])
-
-
-def chat_component(
-    gpt_agent: GPTAgent,
-    user_icon: str = FRONTEND_USER_ICON,
-    bot_icon: str = FRONTEND_BOT_ICON,
-) -> None:
-    # INITIALIZE SESSION STATES #######################################################################################
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = []
-    if "follow_ups" not in st.session_state:
-        st.session_state["follow_ups"] = []
-
-    if "pending_user_input" not in st.session_state:
-        st.session_state["pending_user_input"] = ""
-
-    # Show existing messages
-    display_conversation_history(user_icon, bot_icon)
-
-    chat_text = st.chat_input("Inserisci il tuo messaggio qui...")
-
-    # The user has clicked on a follow up, use the follow up as chat input to query the model
-    if st.session_state["pending_user_input"]:
-        chat_text = st.session_state["pending_user_input"]
-        st.session_state["pending_user_input"] = ""
-
-    # if the user has selected a follow up (chat_text = pending_user_input) or has sent a message
-    if chat_text:
-        st.session_state["messages"].append({"role": "user", "content": chat_text})
-
-        display_user_message_to_chat(chat_text, user_icon)
-        process_and_display_gpt_streaming_response(gpt_agent, bot_icon)
-
-        generate_follow_up_suggestion(gpt_agent)
-
-    handle_and_display_follow_up_suggestions()
-
-
-def main(
-    oic_logo_url: str = FRONTEND_OIC_LOGO_URL,
-    user_icon: str = FRONTEND_USER_ICON,
-    bot_icon: str = FRONTEND_BOT_ICON,
-):
-    st.set_page_config(page_title="OiC Bot", page_icon=oic_logo_url)
-
-    gpt_agent = GPTAgent(
-        st.secrets["AZURE_OPENAI_API_KEY"],
-        st.secrets["AZURE_OPENAI_ENDPOINT"],
-        st.secrets["AZURE_OPENAI_MODEL_NAME"],
-    )
-
+def display_logo(oic_logo_url: str) -> None:
     st.markdown(
         FRONTEND_HTML_STYLES,
         unsafe_allow_html=True,
@@ -132,8 +34,28 @@ def main(
         unsafe_allow_html=True,
     )
 
-    # chat ############################################################################################################
-    chat_component(gpt_agent, user_icon, bot_icon)
+
+def main(
+    oic_logo_url: str = FRONTEND_OIC_LOGO_URL,
+    user_icon: str = FRONTEND_USER_ICON,
+    bot_icon: str = FRONTEND_BOT_ICON,
+):
+    st.set_page_config(page_title="OiC Bot", page_icon=oic_logo_url)
+
+    gpt_agent = GPTAgent(
+        st.secrets["AZURE_OPENAI_API_KEY"],
+        st.secrets["AZURE_OPENAI_ENDPOINT"],
+        st.secrets["AZURE_OPENAI_MODEL_NAME"],
+    )
+    display_logo(oic_logo_url)
+
+    if "page" not in st.session_state:
+        st.session_state.page = "questions"
+
+    if st.session_state.page == "questions":
+        user_questioner()
+    elif st.session_state.page == "chat":
+        chat_component(gpt_agent, user_icon, bot_icon)
 
 
 if __name__ == "__main__":
---
+++ src/frontend_components/questionnaire.py
@@ -0,0 +1,109 @@
+import streamlit as st
+from src.frontend_components.utils import show_selected_answers
+
+def create_pills(
+    question: str, option_map: dict[int, str], selection_mode: str = "multi"
+) -> str | list[str]:
+    return st.pills(
+        question,
+        options=option_map,
+        format_func=lambda option: option_map[option],
+        selection_mode=selection_mode,
+        key=",".join(option_map.values()),
+    )
+
+def verify_and_show_selected_answers(answers: dict) -> None:
+    if (
+        answers["shop_type"] is not None
+        and answers["age"] is not None
+        and answers["communication_type"] is not None
+        and answers["occasion"] is not None
+    ):
+        if st.button("Invia"):
+            st.session_state.answers = answers
+            st.session_state.page = "chat"
+            st.rerun()
+    else:
+        st.write("**Compila tutte le domande per abilitare il pulsante di invio.**")
+
+
+def user_questioner():
+    answers = {}
+    st.subheader(
+        "Questo questionario raccoglie informazioni per generare messaggi WhatsApp per i clienti in diverse occasioni."
+    )
+
+    st.write("1. Quale tipo di ottico sei?")
+    shop_type_map = {
+        0: "Lusso",
+        1: "Bambini",
+        2: "Sportivo",
+        3: "Tecnico",
+        4: "Sole",
+    }
+    shop_type_selected_keys = create_pills(
+        "Seleziona una o più opzioni:", shop_type_map
+    )
+    answers["shop_type"] = (
+        [shop_type_map[key] for key in shop_type_selected_keys]
+        if shop_type_selected_keys
+        else None
+    )
+
+    # Domanda 2: Fascia d'età del target
+    st.write("2. Qual è la fascia d'età del pubblico target di questa comunicazione?")
+    age_map = {
+        0: "0-10 anni",
+        1: "11-18 anni",
+        2: "19-30 anni",
+        3: "31-50 anni",
+        4: "51-65 anni",
+        5: "65+ anni",
+    }
+    age_selected_keys = create_pills("Seleziona una o più fasce d'età:", age_map)
+    answers["age"] = (
+        [age_map[key] for key in age_selected_keys] if age_selected_keys else None
+    )
+
+    # Domanda 3: Tono di voce
+    st.write("3. Qual è il tono di voce di questa comunicazione?")
+    communication_type_map = {
+        0: "Professionale",
+        1: "Amichevole",
+        2: "Informale",
+        3: "Formale",
+        4: "Elegante",
+        5: "Tecnico",
+    }
+    communication_type_selected_key = create_pills(
+        "Seleziona una opzione:", communication_type_map, selection_mode="single"
+    )
+    answers["communication_type"] = (
+        communication_type_map[communication_type_selected_key]
+        if isinstance(communication_type_selected_key, int)
+        else None
+    )
+
+    st.write("4. Qual è l'occasione di questa comunicazione?")
+    occasion_map = {
+        0: "Promozione speciale",
+        1: "Promemoria appuntamento",
+        2: "Nuovo prodotto disponibile",
+        3: "Ringraziamento per la fedeltà",
+        4: "Promozione stagionale",
+        5: "Compleanno",
+    }
+    occasion_selected_key = create_pills(
+        "Seleziona una opzione:", occasion_map, selection_mode="single"
+    )
+    answers["occasion"] = (
+        occasion_map[occasion_selected_key]
+        if isinstance(occasion_selected_key, int)
+        else None
+    )
+
+    st.subheader("Le tue risposte")
+
+    show_selected_answers(answers)
+
+    verify_and_show_selected_answers(answers)
